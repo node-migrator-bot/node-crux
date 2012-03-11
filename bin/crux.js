c0db788c92cfeb9fedb9bd25b024e8d7568574ed
James Brumond <kbjr14@gmail.com>
2012-03-10 19:24:21 -0800

    working on `crux patch`

--- bin/crux.js
+++ bin/crux.js
@@ -2,17 +2,20 @@
 
 var fs      = require('fs');
 var path    = require('path');
+var git     = require('gitjs');
 var wrench  = require('wrench');
 
 var BASE_PATH      = path.join(__dirname, '..');
 var TEMPLATE_PATH  = path.join(BASE_PATH, 'template');
 var PID_FILE       = '.server-pid';
+var PATCH_REMOTE   = '__crux_patch';
+var GITHUB_REPO    = 'git@github.com:kbjr/node-crux';
 
 var args = process.argv.slice(2);
 
 switch (args.shift()) {
 	
-	// builder init [directory]
+	// crux init [directory]
 	case 'init':
 		var creationPath = path.join(process.cwd(), (args[0] || '.'));
 		// Make sure the template exists
@@ -29,7 +32,7 @@ switch (args.shift()) {
 		});
 	break;
 	
-	// builder start [--quiet]
+	// crux start [--quiet]
 	case 'start':
 		var projectPath = findUpTree(process.cwd(), 'core/init.js');
 		process.chdir(projectPath);
@@ -41,7 +44,7 @@ switch (args.shift()) {
 		});
 	break;
 	
-	// builder migrate [up|down|create]
+	// crux migrate [up|down|create]
 	case 'migrate':
 		var migratePath = path.join(BASE_PATH, 'node_modules/migrate/bin/migrate');
 		var projectPath = findUpTree(process.cwd(), 'migrations', function(file) {
@@ -51,22 +54,103 @@ switch (args.shift()) {
 		require('./migrate').run(args, projectPath);
 	break;
 	
-	// builder npm [...]
+	// crux npm [...]
 	case 'npm':
-		var projectPath = findUpTree(process.cwd(), 'core/init.js');
-		process.chdir(projectPath);
+		changeDirectoryToProjectPath();
 		var quiet =!! (args[0] && args[0] === '--quiet' && args.shift());
 		args.unshift('npm');
 		runProcess('/usr/bin/env', args, quiet);
 	break;
 	
-	// builder [...]
+	// crux patch <commit-ish>
+	case 'patch':
+		changeDirectoryToProjectPath();
+		var quiet =!! (args[0] && args[0] === '--quiet' && args.shift());
+		
+		if (! quiet) {
+			console.log('> Preparing to patch...');
+		}
+
+		// Open the git repo
+		git.open('.', true, throws(function(repo, autoCreated) {
+			
+			// If the repo did not already exist, make sure we commit all the files
+			if (autoCreated) {
+				repo.add('*', throws(function() {
+					repo.commit('foo', throws(function() {
+						afterOpen();
+					}));
+				}));
+			} else {
+				afterOpen();
+			}
+			
+			// After init-ing and possibily commiting, make sure we have a remote
+			function afterOpen() {
+				repo.remoteExists(PATCH_REMOTE, throws(function(exists) {
+					if (exists) {
+						return doCherrypick();
+					}
+					repo.run('remote add ? ?', [PATCH_REMOTE, GITHUB_REPO], throws(doCherrypick));
+				}));
+			}
+			
+			// Once we are sure the remote exists, fetch and cherry-pick
+			function doCherrypick() {
+				if (! quiet) {
+					console.log('> Fetching patch data from repository...');
+				}
+				repo.run('fetch ?', [PATCH_REMOTE], throws(function() {
+					repo.run('cherry-pick ?', [args.join(' ')], throws(function() {
+						
+						// If we created the repo, we can now destroy it
+						if (autoCreated) {
+							var _git = path.join(process.cwd(), '.git';
+							wrench.rmdirRecursive(_git, throws(done));
+						} else {
+							done();
+						}
+						
+						function done() {
+							console.log('> Patching complete');
+						}
+						
+					}));
+				}));
+			}
+			
+		});
+	break;
+	
+	// crux [...]
 	default:
 		console.log('Invalid use');
 	break;
 	
 }
 
+// ------------------------------------------------------------------
+//  Internals
+
+// If a first parameter is given, throw it
+function throws(callback) {
+	return function(err) {
+		if (err) {throw err;}
+		if (typeof callback === 'function') {
+			var args = Array.prototype.slice.call(arguments, 1);
+			return callback.apply(this, args);
+		}
+	}
+}
+
+// Change to the directory at the project root
+function changeDirectoryToProjectPath() {
+	process.chdir(
+		findUpTree(process.cwd(), 'core/init.js')
+	);
+}
+
+// Run a child process
 function runProcess(cmd, args, quiet, logFile, onkill) {
 	var proc = require('child_process').spawn(cmd, args);
 	var toLogFile;
@@ -125,4 +209,4 @@ function findUpTree(findIn, find, otherTest) {
 	}
 }
 
-/* End of file builder.js */
+/* End of file crux.js */
